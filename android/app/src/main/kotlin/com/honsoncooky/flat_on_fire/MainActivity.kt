86bfc45c21dc20a3c0b1f2537d9b90392e95375f
HonsonCooky <HarrisonCook13@gmail.com>
2022-06-14 09:10:04 +1200

    Implement App Check

--- android/app/src/main/kotlin/com/honsoncooky/flat_on_fire/MainActivity.kt
+++ android/app/src/main/kotlin/com/honsoncooky/flat_on_fire/MainActivity.kt
@@ -1,6 +1,29 @@
 package com.honsoncooky.flat_on_fire
 
+import androidx.annotation.NonNull
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+    private val CHANNEL = "honsoncooky.flutter.dev/appcheck"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                if (call.method == "installDebug") {
+                    val firebaseAppCheck = FirebaseAppCheck.getInstance()
+                    firebaseAppCheck.installAppCheckProviderFactory(
+                        DebugAppCheckProviderFactory.getInstance()
+                    )
+                    result.success(1)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flat_on_fire/common_models/auth_model.dart';
 import 'package:flat_on_fire/common_models/view_model.dart';
 import 'package:flat_on_fire/firebase_options.dart';
 import 'package:flat_on_fire/routing/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate();
+  
   GetIt.I.registerSingleton<AuthModel>(AuthModel());
   GetIt.I.registerSingleton<ViewModel>(ViewModel());
+
+  // Allow emulator to pass APP CHECK
+  // Whitelist environment means that this is safe to upload, as it will only allow known emulators to pass.
+  if (kDebugMode) {
+    const platform = MethodChannel('honsoncooky.flutter.dev/appcheck');
+    await platform.invokeMethod("installDebug");
+  }
   runApp(App());
 }
 
